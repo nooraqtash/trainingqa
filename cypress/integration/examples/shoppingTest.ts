ac3dad6fbf7cfab4d4fde0f482282663b36d5a10
nooraqtash <noor.alaqtash@optimumpartners.co>
2023-11-28 23:35:08 +0300

    removing spaces

--- cypress/integration/examples/shoppingTest.ts
+++ cypress/integration/examples/shoppingTest.ts
@@ -24,7 +24,7 @@ describe('shoppingTest', () => {
         //checking if the items added are correct
         cy.get(saucedemoPage.firstItemInCart).should('include.text','Backpack')
         cy.get(saucedemoPage.lastItemInCart).should('include.text','T-Shirt')
-        
+
 
         //remove item from cart
         cy.get(saucedemoPage.removeButtonforFirstItem).click()
@@ -32,20 +32,20 @@ describe('shoppingTest', () => {
         //checking that it is removed without adding it to the page object
         cy.get(saucedemoPage.firstItemInCart).should('not.exist')
 
-        //check out 
+        //check out
         cy.get(saucedemoPage.checkOutButton).click();
         //Filling in the details
 
         cy.get(saucedemoPage.firstNameField).type(data1.firstName)
         cy.get(saucedemoPage.lastNameField).type(data1.lastName)
         cy.get(saucedemoPage.postCodeField).type(data1.zipCode)
-        
+
         cy.get(saucedemoPage.submitFormButton).click();
 
         //checking the overview then moving to the next page
         cy.get(saucedemoPage.overview).should('include.text', 'Overview');
         cy.get(saucedemoPage.checkOutButton).click();
-        
+
         //Checking the successful message text
         cy.get(saucedemoPage.header).should('have.text', 'THANK YOU FOR YOUR ORDER');
     });
